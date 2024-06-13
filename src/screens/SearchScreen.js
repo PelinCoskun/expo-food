c33c56d80d4dea4c0d54a818f93cb30f96495367
PelinCoskun <pelinefpit@gmail.com>
2024-06-13 17:14:24 +0300

    rf

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,6 +7,7 @@ const SearchScreen = () =>{
 
     const [term,setTerm]=useState('');
     const [results,setResults]=useState([]);
+    const [errorMessage,setErrorMessage]=useState('');
     const searchApi=async() => {
       try{
       const response = await yelp.get('/search',{
@@ -18,13 +19,13 @@ const SearchScreen = () =>{
       });
       setResults(response.data.businesses);
     } catch (err){
-      console.log(err)
+     setErrorMessage('Somethinh went wrong')
     }
   }
 
     return <View>
         <SearchBar  term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-        <Text>Search Screen </Text>
+        {errorMessage ? <Text>{errorMessage} </Text> : null}
         <Text>We have found {results.length} results</Text>
     </View>
 };
