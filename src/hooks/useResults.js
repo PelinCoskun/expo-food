fb4965a458111011a108fd8c02814036ddb5a737
PelinCoskun <pelinefpit@gmail.com>
2024-06-14 11:15:06 +0300

    ew

---
+++ src/hooks/useResults.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+ 
+export default ()=>{
+
+    const [results,setResults]=useState([]);
+    const [errorMessage,setErrorMessage]=useState('');
+    const searchApi=async() => {
+      console.log('Hi there!')
+      try{
+      const response = await yelp.get('/search',{
+        params:{
+            limit:50,
+            term,
+            location:'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err){
+     setErrorMessage('Somethinh went wrong')
+    }
+  }
+
+    useEffect(()=>{
+      searchApi('pasta')
+    },[searchApi,results,errorMessage])
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () =>{
 
     const [term,setTerm]=useState('');
-    const [results,setResults]=useState([]);
-    const [errorMessage,setErrorMessage]=useState('');
-    const searchApi=async() => {
-      try{
-      const response = await yelp.get('/search',{
-        params:{
-            limit:50,
-            term,
-            location:'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err){
-     setErrorMessage('Somethinh went wrong')
-    }
-  }
 
     return <View>
         <SearchBar  term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
